ec13315c77df44ab3002521bcb3e52cd9972fca0
Madison-k <72760208+Madison-k@users.noreply.github.com>
2020-11-24 11:16:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+while (true) {
+    console.log("temperature" + input.temperature(TemperatureUnit.Fahrenheit))
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 80) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 40) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else {
+        light.setAll(light.rgb(0, 255, 0))
+    }
+    
+}
